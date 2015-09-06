3007dba451720a4d74e497c98f82e172abc5911f
ezadez <ezadez@gmail.com>
2015-09-06 20:40:35 +0900

    add express

---
+++ animal.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var MongoClient = require('mongodb').MongoClient;
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+var server = app.listen(3000, 'localhost', function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Animal app listening at http://%s:%s', host, port);
+});
